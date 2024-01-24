ec4064c3649bbaa7cdbb72004d0fabb8009d2ed9
Vamsi Saripudi <120817655+vamsisaripudi7514@users.noreply.github.com>
2024-01-24 10:36:31 +0530

    Update India.jsx

--- frontend/src/components/India.jsx
+++ frontend/src/components/India.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { Overlay, Tooltip } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  './india.css';
 const India = () => {
